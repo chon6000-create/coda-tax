cf19f510c6cf48502dd1eebd1b49e926e65d3f0c
JIIN <you@example.com>
2026-02-23 13:10:58 +0900

    chore: clean up build files and stabilize asset names for pwa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: '/coda-tax/',
     publicDir: 'public',
+    build: {
+        rollupOptions: {
+            output: {
+                // Disable hashing for predictable filenames in sw.js
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`
+            }
+        }
+    },
     server: {
         port: 5173,
         open: true
